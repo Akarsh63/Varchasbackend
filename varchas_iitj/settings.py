60dc3749e68b53fd78a6a23d809d5800eeccd36e
Akarsh63 <akarsh.1@iitj.ac.in>
2023-10-05 19:03:01 +0530

    ff

--- varchas_iitj/settings.py
+++ varchas_iitj/settings.py
@@ -186,7 +186,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100
 }
-
-CSRF_COOKIE_SECURE = True
 CSRF_COOKIE_NAME = 'csrftoken'
 CSRF_HEADER_NAME = 'HTTP_X_CSRFTOKEN'
