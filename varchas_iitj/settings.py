a9c2e10cadd41a0fce120ffe6dbb96a37637e19c
Sai Mani Akarsh Rankireddy <104289962+Akarsh63@users.noreply.github.com>
2023-10-06 19:31:33 +0530

    Update settings.py

--- varchas_iitj/settings.py
+++ varchas_iitj/settings.py
@@ -172,17 +172,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 APPEND_SLASH = True
 
-# EMAIL_HOST = config('EMAIL_HOST')
-# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-# EMAIL_PORT = 465
-# EMAIL_USE_SSL = True
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER =varchas2023iitj@gmail.com
-EMAIL_HOST_PASSWORD = pbuggrvcusemrkdp
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = config('EMAIL_BACKEND')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
