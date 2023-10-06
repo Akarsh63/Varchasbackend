0f5c751f4ffa9adf39e6e50418fe55d0d24fa033
Sai Mani Akarsh Rankireddy <104289962+Akarsh63@users.noreply.github.com>
2023-10-06 17:33:49 +0530

    Update settings.py

--- varchas_iitj/settings.py
+++ varchas_iitj/settings.py
@@ -186,7 +186,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100
 }
-CSRF_COOKIE_NAME = 'csrftoken'
-CSRF_COOKIE_DOMAIN='http://localhost:3000'
-SESSION_COOKIE_DOMAIN='http://localhost:3000'
-CSRF_HEADER_NAME = 'HTTP_X_CSRFTOKEN'
+# CSRF_COOKIE_NAME = 'csrftoken'
+# CSRF_COOKIE_DOMAIN='http://localhost:3000'
+# SESSION_COOKIE_DOMAIN='http://localhost:3000'
+# CSRF_HEADER_NAME = 'HTTP_X_CSRFTOKEN'
