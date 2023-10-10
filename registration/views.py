8c74fd95b1e15bcdcfb651b69fe4b05c4780b2ec
Sai Mani Akarsh Rankireddy <104289962+Akarsh63@users.noreply.github.com>
2023-10-10 23:44:41 +0530

    Update views.py

--- registration/views.py
+++ registration/views.py
@@ -30,7 +30,7 @@ def CreateTeamView(request):
             teams=user_profile.teamId.all()
             for team in teams:
                 if int(team.sport) in [1,2,3,4,5,6,7,8,9,10,11,12] :
-                    message = "You are not able to that team"
+                    message = "You are not able to create the team"
                     message += "\nYou have to register again to join another team. \nContact Varchas administrators."
                     return Response({"message": message}, status=status.HTTP_406_NOT_ACCEPTABLE)
     
