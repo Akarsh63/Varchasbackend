a4bf37164cb0697a6afdfb1154ffb861eb0c8b82
Sai Mani Akarsh Rankireddy <104289962+Akarsh63@users.noreply.github.com>
2023-10-14 16:05:03 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -24,6 +24,7 @@ from django.contrib.auth.hashers import make_password
 
 class RegisterUserView(APIView):
     def post(self, request):
+        print(request.data)
         if len(request.data.get('phone', '')) != 10:
             return Response({"Error": "Phone number must be 10 digits"}, status=status.HTTP_400_BAD_REQUEST)
     
